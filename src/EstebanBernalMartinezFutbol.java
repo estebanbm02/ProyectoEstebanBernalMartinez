f8d0332477d3ba41dffcb08fade6bbcab43ff86c
Vespertino <Vespertino@C05PC16.informatica.comercio>
2022-02-21 17:50:15 +0100

    Primer commit - GetLiga

---
+++ src/EstebanBernalMartinezFutbol.java
@@ -0,0 +1,16 @@
+
+public class EstebanBernalMartinezFutbol {
+	public String liga;
+	public String equipo;
+	public String futbol;
+	
+	public EstebanBernalMartinezFutbol(String liga, String equipo, String futbol) {
+		super();
+		this.liga = liga;
+		this.equipo = equipo;
+		this.futbol = futbol;
+	}
+	
+	public String getLiga() {
+		return liga;
+	}
